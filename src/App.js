0de6d80ee2132d9f17cbe62d3d69314694ee56f2
Code-with-Viv <vyvivek2018@gmail.com>
2023-02-15 11:16:45 +0530

    reactBasicApp1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from "react";
+import "./index.css"
+
 
 function App() {
+  const [count,setCount] = useState(0);
+
+  function increment() {
+      setCount(count+1);
+  }
+  function decrement() {
+    setCount(count-1)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <div className="Box">
+        <div className="count">{count}</div>
+        <button onClick={increment} className="button">+</button>
+        <button onClick={decrement} className="button">-</button>
+        </div>
+        
+    </>
+  )
 }
 
 export default App;
